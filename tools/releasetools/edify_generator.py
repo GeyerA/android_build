171933f5478270b89abc0e3c28be54026dbe7392
GeyerA <calo.geyer@gmail.com>
2014-06-05 21:45:25 +0200

    fix BUILD_PROP_OVERRIDES
    Change-Id: Id127d1c7491cf0e2ac90326328467309f0ba2750

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -98,6 +98,14 @@ class EdifyGenerator(object):
            ) % (device, device)
     self.script.append(cmd)
 
+    cmd = ('assert(' +
+           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s" || '
+           'abort("This package is for \\"%s\\" devices; '
+           'this is a \\"" + getprop("ro.product.device") + "\\".");'
+                         % (i, i, i) for i in device.split(",")]) +
+           ');')
+    self.script.append(self._WordWrap(cmd))
+
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
