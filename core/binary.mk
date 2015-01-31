cf6884a44118af684c67be15fecb4ae14362ca1f
mydongistiny <jaysonedson@gmail.com>
2015-01-31 14:27:14 -0800

    Add modular graphite options.

--- core/binary.mk
+++ core/binary.mk
@@ -102,6 +102,15 @@ ifndef LOCAL_IS_HOST_MODULE
 include $(BUILD_SYSTEM)/krait.mk
 endif
 endif
+
+ifeq ($(GRAPHITE_OPTS),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+include $(BUILD_SYSTEM)/graphite.mk
+endif
+endif
+endif
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
