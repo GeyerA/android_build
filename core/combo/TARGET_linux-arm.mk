17cf23b64467032f0d1e37e5b0464215f9ea167f
GeyerA <calo.geyer@gmail.com>
2015-02-08 15:05:52 +0100

    Revert "Fun with flags"
    This reverts commit f1d21251279eb2fdd6ce8df83332ff002ffe8f64.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -67,67 +67,16 @@ $(combo_2nd_arch_prefix)TARGET_STRIP := $($(combo_2nd_arch_prefix)TARGET_TOOLS_P
 
 $(combo_2nd_arch_prefix)TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O3 \
-                        -pipe \
-			-DNDEBUG \
+$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS :=    -O2 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
-                        -funswitch-loops \
-                        -fno-tree-vectorize \
-                        -fno-inline-functions \
-                        -fgcse-after-reload \
-                        -fno-ipa-cp-clone \
-                        -fno-vect-cost-model \
-			-funsafe-loop-optimizations \
-			-fsection-anchors \
-			-fivopts \
-			-ftree-loop-im \
-			-ftree-loop-ivcanon \
-			-ffunction-sections \
-			-fdata-sections \
-			-frename-registers \
-			-fomit-frame-pointer \
-			-fgcse-sm \
-			-fgcse-las \
-			-fweb \
-			-ftracer \
-			-Wno-error=unused-parameter \
-                        -Wno-error=unused-but-set-variable \
-			-Wno-error=maybe-uninitialized 
+                        -funswitch-loops
 
 # Modules can choose to compile some source as thumb.
 $(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS :=  -mthumb \
-                        -O3 \
-                        -pipe \
+                        -Os \
                         -fomit-frame-pointer \
-                        -fno-strict-aliasing \
-                        -fno-tree-vectorize \
-                        -fno-inline-functions \
-                        -fno-unswitch-loops \
-                        -fgcse-after-reload \
-                        -fno-ipa-cp-clone \
-                        -fno-vect-cost-model \
-			-DNDEBUG \
-			-funsafe-loop-optimizations \
-			-fsection-anchors \
-			-fivopts \
-			-ftree-loop-im \
-			-ftree-loop-ivcanon \
-			-ffunction-sections \
-			-fdata-sections \
-			-funswitch-loops \
-			-frename-registers \
-			-frerun-cse-after-loop \
-			-fgcse-sm \
-			-fgcse-las \
-			-fweb \
-			-ftracer \
-                        -Wno-error=unused-parameter \
-                        -Wno-error=unused-but-set-variable \
-			-Wno-error=maybe-uninitialized \
-			-Wno-error=clobbered \
-			-Wno-error=strict-overflow
-
+                        -fno-strict-aliasing
 
 # Set FORCE_ARM_DEBUGGING to "true" in your buildspec.mk
 # or in your environment to force a full arm build, even for
@@ -195,56 +144,12 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 
 # More flags/options can be added here
 $(combo_2nd_arch_prefix)TARGET_RELEASE_CFLAGS := \
-			-fvisibility-inlines-hidden \
-			-O3 \
 			-DNDEBUG \
-			-funsafe-loop-optimizations \
-			-fsection-anchors \
-			-fivopts \
-			-ftree-loop-im \
-			-ftree-loop-ivcanon \
-			-ffunction-sections \
-			-fdata-sections \
-			-funswitch-loops \
-			-pipe \
-			-fomit-frame-pointer \
-			-fgcse-sm \
-			-fgcse-las \
-			-fweb \
-			-ftracer \
-			-Wno-error=unused-parameter \
-			-Wno-error=unused-but-set-variable \
-			-Wno-error=maybe-uninitialized
-
-# More flags/options can be added here
-$(combo_2nd_arch_prefix)TARGET_RELEASE_CFLAGS := \
-			-O3 \
-			-DNDEBUG \
-                        -pipe \
 			-g \
 			-Wstrict-aliasing=2 \
 			-fgcse-after-reload \
 			-frerun-cse-after-loop \
-			-frename-registers \
-			-fno-ipa-cp-clone \
-			-fno-vect-cost-model \
-			-fno-strict-aliasing \
-			-funsafe-loop-optimizations \
-			-fsection-anchors \
-			-fivopts \
-			-ftree-loop-im \
-			-ftree-loop-ivcanon \
-			-ffunction-sections \
-			-fdata-sections \
-			-funswitch-loops \
-			-fomit-frame-pointer \
-			-fgcse-sm \
-			-fgcse-las \
-			-fweb \
-			-ftracer \
-			-Wno-error=unused-parameter \
-			-Wno-error=unused-but-set-variable \
-			-Wno-error=maybe-uninitialized
+			-frename-registers
 
 libc_root := bionic/libc
 libm_root := bionic/libm
--- core/combo/select.mk
+++ core/combo/select.mk
@@ -49,10 +49,10 @@ $(combo_var_prefix)HAVE_STRLCPY := 0
 $(combo_var_prefix)HAVE_STRLCAT := 0
 $(combo_var_prefix)HAVE_KERNEL_MODULES := 0
 
-$(combo_var_prefix)GLOBAL_CFLAGS := -O3 -DNDEBUG -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -fno-exceptions -Wno-multichar -pipe
-$(combo_var_prefix)RELEASE_CFLAGS := -O3 -DNDEBUG -fno-strict-aliasing -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -pipe
-$(combo_var_prefix)GLOBAL_CPPFLAGS := -O3 -DNDEBUG -fivopts -ffunction-sections -fdata-sections -funswitch-loops -pipe -fomit-frame-pointer -ftracer
-$(combo_var_prefix)GLOBAL_LDFLAGS := 
+$(combo_var_prefix)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
+$(combo_var_prefix)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
+$(combo_var_prefix)GLOBAL_CPPFLAGS :=
+$(combo_var_prefix)GLOBAL_LDFLAGS :=
 $(combo_var_prefix)GLOBAL_ARFLAGS := crsPD
 $(combo_var_prefix)GLOBAL_LD_DIRS :=
 
